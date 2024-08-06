fb3cc1414135004d439b16d6902e442957f1223f
CLINTON <issnasty2@gmail.com>
2024-08-06 13:53:16 +0100

    frontend updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,17 +7,17 @@ function LandingPage() {
 
     return (
         <div className="landing-page">
-            <div className="title-cards">
-                <div className="card" onClick={() => navigate('/login/ceo')}>
+            <div id="landing-card">
+                <div className="landing-card" onClick={() => navigate('/login/ceo')}>
                     <h2>I am the CEO</h2>
                 </div>
-                <div className="card" onClick={() => navigate('/login/finance-manager')}>
+                <div className="landing-card" onClick={() => navigate('/login/finance-manager')}>
                     <h2>I am the Finance Manager</h2>
                 </div>
-                <div className="card" onClick={() => navigate('/login/procurement-manager')}>
+                <div className="landing-card" onClick={() => navigate('/login/procurement-manager')}>
                     <h2>I am the Procurement Manager</h2>
                 </div>
-                <div className="card" onClick={() => navigate('/login/tenant')}>
+                <div className="landing-card" onClick={() => navigate('/login/tenant')}>
                     <h2>I am a Tenant</h2>
                 </div>
             </div>
